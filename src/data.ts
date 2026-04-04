4bbaeae461efd6865a715d265fc092c71984333c
jjlmoya <juanjo1989@gmail.com>
2026-04-04 15:59:40 +0200

    feat: initial scaffolding for @jjlmoya/utils-hardware

---
+++ src/data.ts
@@ -0,0 +1,11 @@
+export { hardwareCategory } from './category';
+
+export type {
+  KnownLocale,
+  ToolLocaleContent,
+  CategoryLocaleContent,
+  LocaleMap,
+  HardwareToolEntry,
+  HardwareCategoryEntry,
+} from './types';
+
---
+++ src/index.ts
@@ -0,0 +1,19 @@
+export { hardwareCategory } from './category';
+export { default as hardwareCategorySEO } from './category/seo.astro';
+
+export type {
+  KnownLocale,
+  FAQItem,
+  BibliographyEntry,
+  HowToStep,
+  ToolLocaleContent,
+  CategoryLocaleContent,
+  LocaleLoader,
+  LocaleMap,
+  HardwareToolEntry,
+  HardwareCategoryEntry,
+  ToolDefinition,
+} from './types';
+
+export { ALL_TOOLS } from './tools';
+
